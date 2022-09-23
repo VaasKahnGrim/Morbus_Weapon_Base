4ae0512cc5cfccbb1abd43b7b633ddeb9b9686e7
Vaas_Kahn_Grim <29955667+VaasKahnGrim@users.noreply.github.com>
2022-09-23 15:36:08 -0400

    Update sh_ammo.lua

--- lua/weapons/weapon_mor_base/sh_ammo.lua
+++ lua/weapons/weapon_mor_base/sh_ammo.lua
@@ -1,1 +1,17 @@
---not implemented yet
+--not fully implemented yet
+local AmmoTypes = {
+	["Default"] = 10,
+	["9mm"] = 5,
+	["5.56"] = 15,
+	["10mm"] = 8,
+	["7.62"] = 10,
+	["Electic"] = 8,
+	["Electric+"] = 20,
+	["Fire"] = 8
+}
+
+function SWEP:SetupDamage()
+	self.Primary.Damage = AmmoTypes[self.Primary.Round] or AmmoTypes.Default or 10
+end
+
+
