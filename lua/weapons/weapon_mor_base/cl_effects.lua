e1d599c0d60ef714e5c852f021f1e0b18ce231d9
Vaas_Kahn_Grim <29955667+VaasKahnGrim@users.noreply.github.com>
2022-09-23 15:44:43 -0400

    Update cl_effects.lua

--- lua/weapons/weapon_mor_base/cl_effects.lua
+++ lua/weapons/weapon_mor_base/cl_effects.lua
@@ -1,1 +1,19 @@
---not implemented yet
+--not fully implemented yet
+local ImpactTypes = {	--Change these asap
+	["Default"] = "fadingscorch",
+	["9mm"] = "fadingscorch",
+	["5.56"] = "fadingscorch",
+	["10mm"] = "fadingscorch",
+	["7.62"] = "fadingscorch",
+	["Electic"] = "fadingscorch",
+	["Electric+"] = "fadingscorch",
+	["Fire"] = "fadingscorch"
+}
+
+function SWEP:DoImpactEffect(tr, dmgtype)
+	if tr.HitSky then return true end
+	util.Decal(ImpactTypes[self.Round] or ImpactTypes.Default, tr.HitPos + tr.HitNormal, tr.HitPos - tr.HitNormal)
+	return true
+end
+
+
