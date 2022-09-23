8a779009ccf32e7b23c395299dc878845023e491
Vaas_Kahn_Grim <29955667+VaasKahnGrim@users.noreply.github.com>
2022-09-23 14:58:07 -0400

    Update sh_modes.lua

--- lua/weapons/weapon_mor_base/sh_modes.lua
+++ lua/weapons/weapon_mor_base/sh_modes.lua
@@ -1,1 +1,25 @@
---not implemented yet
+--not fully implemented yet
+--Firemode ENUMs
+FIREMODE_SEMI = 0
+FIREMODE_AUTO = 1
+FIREMODE_BURST = 2
+FIREMODE_SAFETY = 3
+
+SWEP.FireMode = 0
+SWEP.FireModes = {
+  [FIREMODE_SAFETY] = true  --All weapons have this firemode
+}
+
+function SWEP:SetFireMode(mode)
+    self.FireModes = self.FireModes[mode] and mode or self.FireModes
+end
+
+function SWEP:GetFireMode()
+  return self.FireMode or 0
+end
+
+function SWEP:IsSafetyOn()
+  return (self.FireMode == 3) or false
+end
+
+
