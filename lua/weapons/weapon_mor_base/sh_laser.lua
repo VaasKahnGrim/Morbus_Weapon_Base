1aa81e49713e0faaf5825653629dfa945f38bb35
Vaas_Kahn_Grim <29955667+VaasKahnGrim@users.noreply.github.com>
2022-09-26 03:49:31 -0400

    Update sh_laser.lua

--- lua/weapons/weapon_mor_base/sh_laser.lua
+++ lua/weapons/weapon_mor_base/sh_laser.lua
@@ -1,9 +1,11 @@
 --not 100% tested
 hook.Add( "PlayerButtonDown", "TogleLaser", function( ply, button )
-		if IsFirstTimePredicted() and button == KEY_L then
-        local wep = ply:GetActiveWeapon()
-        if wep.ToggleLaser then
-            wep:ToggleLaser()
-        end
-    end
+	if IsFirstTimePredicted() and button == KEY_L then
+		local wep = ply:GetActiveWeapon()
+		if wep.ToggleLaser then
+			wep:ToggleLaser()
+		end
+	end
 end)
+
+
